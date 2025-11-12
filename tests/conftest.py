32ed35b5f4e813bc660e5a841943c822f668a994
Zuzanna Bąk <zuzanna.bak@temple.edu>
2025-11-12 16:42:55 -0500

    changes to improve github readibility

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ import pytest
 @pytest.fixture(autouse=True)
 def patch_unit_costs(monkeypatch):
     monkeypatch.setattr(
-        "api.k_resource.unit_costs",
+        "cucal.api.k_resource.unit_costs",
         lambda ids: {rid: 42.0 for rid in ids},  # constant price
     )
